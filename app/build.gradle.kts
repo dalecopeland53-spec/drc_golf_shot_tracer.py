f8e84fe431693c5192f6dbf8f0c4c375f16d9048
dalecopeland53-spec <dale.copeland53@gmail.com>
2026-09-10 22:56:16 +1000

    Add tracker Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.bettergolf.app"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.bettergolf.app"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
